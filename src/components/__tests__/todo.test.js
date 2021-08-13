1eb05552e9403af05a98e119c59b90397e362586
GOPICHAND Ravikanti <ravikanti.gopichand@soprasteria.com>
2021-08-13 18:01:41 +0530

    testing array

--- src/components/__tests__/todo.test.js
+++ src/components/__tests__/todo.test.js
@@ -1,18 +1,19 @@
 import { render, screen, cleanup } from '@testing-library/react'
 import Todo from '../todo';
 test('should render todo component', () => {
-    const todo = { id: 1, title: "bring grocery", status: false }
+    const todos = { id: 1, title: "bring grocery", status: false }
     render(<Todo todo={todo} />);
     const todoElement = screen.getAllByAltText('todo-1');
     expect(todoElement).toBeInTheDocument();
     expect(todoElement).toHaveTextContext('bring grocery');
+    expect(todoElement).toContainHTML('<strike');
 
 })
 test('should render todo component', () => {
-    const todo = { id: 2, title: "wash dishes", status: false }
+    const todos = { id: 2, title: "wash dishes", status: false }
     render(<Todo todo={todo} />);
     const todoElement = screen.getAllByAltText('todo-2');
     expect(todoElement).toBeInTheDocument();
     expect(todoElement).toHaveTextContext('wash dishes');
-
+    expect(todoElement).toContainHTML('<strike');
 })
