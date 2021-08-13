1bd35058e68231b5d5da7874f55f9058471beb72
GOPICHAND Ravikanti <ravikanti.gopichand@soprasteria.com>
2021-08-13 16:01:28 +0530

    testing arrrangements

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Button() {
+    return (
+        <div data-testid="button">
+            {Label}
+            
+        </div>
+    )
+}
+
+export default Button
---
+++ src/components/todo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Todo()
+{
+    return (
+        <div data-testid="todo-1">
+
+            hello world
+            
+        </div>
+    )
+}
+export default Todo
